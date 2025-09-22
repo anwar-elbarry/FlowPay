49343ffa3685bcbdee52b7f2253ccd81204be302
Anouar El barry <elbarryanwar37@gmail.com>
2025-09-22 17:07:19 +0100

    fit:generate getters and setters

--- src/entity/Abonnement.java
+++ src/entity/Abonnement.java
@@ -11,4 +11,52 @@ public abstract class Abonnement {
     protected LocalDateTime dateDebut;
     protected LocalDateTime dateFin;
     protected AbnStatut statut;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNomService() {
+        return nomService;
+    }
+
+    public void setNomService(String nomService) {
+        this.nomService = nomService;
+    }
+
+    public double getMontantMensuel() {
+        return montantMensuel;
+    }
+
+    public void setMontantMensuel(double montantMensuel) {
+        this.montantMensuel = montantMensuel;
+    }
+
+    public LocalDateTime getDateFin() {
+        return dateFin;
+    }
+
+    public void setDateFin(LocalDateTime dateFin) {
+        this.dateFin = dateFin;
+    }
+
+    public LocalDateTime getDateDebut() {
+        return dateDebut;
+    }
+
+    public void setDateDebut(LocalDateTime dateDebut) {
+        this.dateDebut = dateDebut;
+    }
+
+    public AbnStatut getStatut() {
+        return statut;
+    }
+
+    public void setStatut(AbnStatut statut) {
+        this.statut = statut;
+    }
 }
--- src/entity/Paiement.java
+++ src/entity/Paiement.java
@@ -12,4 +12,51 @@ public class Paiement {
     private String typePaiement;
     private PayStatut statut;
 
+    public String getIdPaiement() {
+        return idPaiement;
+    }
+
+    public void setIdPaiement(String idPaiement) {
+        this.idPaiement = idPaiement;
+    }
+
+    public Abonnement getIdAbonnement() {
+        return idAbonnement;
+    }
+
+    public void setIdAbonnement(Abonnement idAbonnement) {
+        this.idAbonnement = idAbonnement;
+    }
+
+    public LocalDateTime getDateEcheance() {
+        return dateEcheance;
+    }
+
+    public void setDateEcheance(LocalDateTime dateEcheance) {
+        this.dateEcheance = dateEcheance;
+    }
+
+    public LocalDateTime getDatePaiement() {
+        return datePaiement;
+    }
+
+    public void setDatePaiement(LocalDateTime datePaiement) {
+        this.datePaiement = datePaiement;
+    }
+
+    public String getTypePaiement() {
+        return typePaiement;
+    }
+
+    public void setTypePaiement(String typePaiement) {
+        this.typePaiement = typePaiement;
+    }
+
+    public PayStatut getStatut() {
+        return statut;
+    }
+
+    public void setStatut(PayStatut statut) {
+        this.statut = statut;
+    }
 }
