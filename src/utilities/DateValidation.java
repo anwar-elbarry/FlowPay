e4685f184b92e58049de0fd90d43e00700fd4e68
Anouar El barry <elbarryanwar37@gmail.com>
2025-09-24 11:09:05 +0100

    create date validation

--- src/utilities/DateValidation.java
+++ src/utilities/DateValidation.java
@@ -1,4 +1,20 @@
 package utilities;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
+import java.util.Scanner;
+
 public class DateValidation {
+    private static final Scanner sc = new Scanner(System.in);
+    public static LocalDate askDate(String message){
+        while (true){
+            System.out.println(message);
+            String input  = sc.nextLine();
+            try {
+                return LocalDate.parse(input);
+            }catch (DateTimeParseException e){
+                System.out.println("❌ Format invalide. Réessayez (ex: 2025-09-23).");
+            }
+        }
+    }
 }
