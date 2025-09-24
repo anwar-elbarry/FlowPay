56f93fa9c2627290c3a1d0a15b14ebb13ab4fccf
Anouar El barry <elbarryanwar37@gmail.com>
2025-09-24 11:08:39 +0100

    create database connection

--- src/utilities/DatabaseConnection.java
+++ src/utilities/DatabaseConnection.java
@@ -1,4 +1,15 @@
 package utilities;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DatabaseConnection {
+    private static final String url = "jdbc:postgresql://localhost:5432/FlowPay";
+    private static final String password = "Jppp5734";
+    private static final String username = "postgres";
+
+    public static Connection getConnection()throws SQLException {
+        return DriverManager.getConnection(url,username,password);
+    }
 }
