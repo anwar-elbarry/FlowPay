e59d0c6c9b599113d0159646fd2cc2f4387febbb
Anouar El barry <elbarryanwar37@gmail.com>
2025-09-25 17:01:22 +0100

    fit:complete create function

--- src/JDBC/PaiementDAOJDBC.java
+++ src/JDBC/PaiementDAOJDBC.java
@@ -3,15 +3,33 @@ package JDBC;
 import DAO.PaiementDAO;
 import entity.Paiement;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
 public class PaiementDAOJDBC implements PaiementDAO {
+    private Connection connection;
+
+    public PaiementDAOJDBC(Connection connection) {
+        this.connection = connection;
+    }
 
     @Override
     public void create(Paiement p) {
-
+        String sql = "INSERT INTO paiement (id_Paiement,id_abonnement, date_echeance, date_paiement, type_paiement,statut) VALUES (?, ?, ?, ?, ? ,?)";
+        try (PreparedStatement pr = connection.prepareStatement(sql)) {
+            pr.setObject(1, p.getIdPaiement());
+            pr.setObject(2, p.getIdAbonnement().getId());
+            pr.setDate(3, java.sql.Date.valueOf(p.getDateEcheance()));
+            pr.setDate(4, java.sql.Date.valueOf(p.getDatePaiement()));
+            pr.setString(5, p.getTypePaiement());
+            pr.setString(6, p.getStatut().name());
+            pr.executeUpdate();
+        } catch (Exception e) {
+            throw new RuntimeException("Erreur lors de la création du paiement : " + e);
+        }
     }
 
     @Override
