5da7d57cdbd9a47e29d8c7a9532517b0ba2681df
Anouar El barry <elbarryanwar37@gmail.com>
2025-09-25 16:28:08 +0100

    fit: add SQLException

--- src/DAO/AbonnementDAO.java
+++ src/DAO/AbonnementDAO.java
@@ -2,15 +2,22 @@ package DAO;
 
 import entity.Abonnement;
 
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
 
 public interface AbonnementDAO {
-     void create(Abonnement a,String type_abonnement);
-     Optional<Abonnement> findById(String id);
-     List<Abonnement> findAll();
-     void modifier(Abonnement a);
-     void supprimer(String id);
-     List<Abonnement> findByType(String type);
-     List<Abonnement> findActiveSubscriptions();
+    void create(Abonnement a, String type_abonnement);
+
+    Optional<Abonnement> findById(String id) throws SQLException;
+
+    List<Abonnement> findAll() throws SQLException;
+
+    void modifier(Abonnement a) throws SQLException;
+
+    void supprimer(String id) throws SQLException;
+
+    List<Abonnement> findByType(String type) throws SQLException;
+
+    List<Abonnement> findActiveSubscriptions() throws SQLException;
 }
