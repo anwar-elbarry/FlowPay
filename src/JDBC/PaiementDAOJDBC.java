479911b3d369e653b552dada81b1124cff6b7f8f
Anouar El barry <elbarryanwar37@gmail.com>
2025-09-25 16:30:04 +0100

    fit:create empty functions to start with

--- src/JDBC/PaiementDAOJDBC.java
+++ src/JDBC/PaiementDAOJDBC.java
@@ -1,4 +1,51 @@
 package JDBC;
 
-public class PaiementDAOJDBC {
+import DAO.PaiementDAO;
+import entity.Paiement;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+public class PaiementDAOJDBC implements PaiementDAO {
+
+    @Override
+    public void create(Paiement p) {
+
+    }
+
+    @Override
+    public Optional<Paiement> findById(String id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Paiement> findAll() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public void modifier(Paiement p) {
+
+    }
+
+    @Override
+    public void supprimer(String id) {
+
+    }
+
+    @Override
+    public List<Paiement> findByAbonnement(String idAbonnement) {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public List<Paiement> findUnpaidByAbonnement(String idAbonnement) {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public List<Paiement> findLastPayments(int Limit) {
+        return Collections.emptyList();
+    }
 }
