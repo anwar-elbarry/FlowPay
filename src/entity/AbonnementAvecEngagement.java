31467543baa926713343238980b91a6d511141bf
Anouar El barry <elbarryanwar37@gmail.com>
2025-09-25 16:26:33 +0100

    fit:create constructor

--- src/entity/AbonnementAvecEngagement.java
+++ src/entity/AbonnementAvecEngagement.java
@@ -1,5 +1,24 @@
 package entity;
 
-public class AbonnementAvecEngagement extends Abonnement{
+import utilities.AbnStatut;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+public class AbonnementAvecEngagement extends Abonnement {
     private int dureeEngagementMois;
+
+    public AbonnementAvecEngagement(UUID id, String nomService, double montantMensuel, LocalDate dateDebut, LocalDate dateFin, AbnStatut statut, int duree_engagement_mois) {
+        super(id, nomService, montantMensuel, dateDebut, dateFin, statut);
+        this.dureeEngagementMois = duree_engagement_mois;
+    }
+
+    public int getDureeEngagementMois() {
+        return dureeEngagementMois;
+    }
+
+    @Override
+    public String toString() {
+        return "AbonnementAvecEngagement [dureeEngagementMois=" + dureeEngagementMois + ", id=" + id + ", nomService=" + nomService + ", montantMensuel=" + montantMensuel + ", dateDebut=" + dateDebut + ", dateFin=" + dateFin + ", statut=" + statut + "]";
+    }
 }
--- src/entity/AbonnementSansEngagement.java
+++ src/entity/AbonnementSansEngagement.java
@@ -1,4 +1,12 @@
 package entity;
 
+import utilities.AbnStatut;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
 public class AbonnementSansEngagement extends Abonnement{
+    public AbonnementSansEngagement(UUID id, String nomService, double montantMensuel, LocalDate dateDebut, LocalDate dateFin, AbnStatut statut) {
+        super(id, nomService, montantMensuel, dateDebut, dateFin, statut);
+    }
 }
