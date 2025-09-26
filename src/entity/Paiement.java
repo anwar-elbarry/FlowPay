b26c23f852f2c3415eadea94789b83eb264589ed
Anouar El barry <elbarryanwar37@gmail.com>
2025-09-26 12:12:13 +0100

    fit:create constructor / create tostring method

--- src/entity/Paiement.java
+++ src/entity/Paiement.java
@@ -3,28 +3,38 @@ package entity;
 import utilities.PayStatut;
 
 import java.time.LocalDate;
+import java.util.UUID;
 
 public class Paiement {
-    private String idPaiement;
-    private Abonnement idAbonnement;
+    private UUID idPaiement;
+    private UUID idAbonnement;
     private LocalDate dateEcheance;
     private LocalDate datePaiement;
     private String typePaiement;
     private PayStatut statut;
 
-    public String getIdPaiement() {
+    public Paiement(UUID idPaiement, LocalDate dateEcheance, UUID idAbonnement, LocalDate datePaiement, String typePaiement, PayStatut statut) {
+        this.idPaiement = idPaiement;
+        this.dateEcheance = dateEcheance;
+        this.idAbonnement = idAbonnement;
+        this.datePaiement = datePaiement;
+        this.typePaiement = typePaiement;
+        this.statut = statut;
+    }
+
+    public UUID getIdPaiement() {
         return idPaiement;
     }
 
-    public void setIdPaiement(String idPaiement) {
+    public void setIdPaiement(UUID idPaiement) {
         this.idPaiement = idPaiement;
     }
 
-    public Abonnement getIdAbonnement() {
+    public UUID getIdAbonnement() {
         return idAbonnement;
     }
 
-    public void setIdAbonnement(Abonnement idAbonnement) {
+    public void setIdAbonnement(UUID idAbonnement) {
         this.idAbonnement = idAbonnement;
     }
 
@@ -58,5 +68,10 @@ public class Paiement {
 
     public void setStatut(PayStatut statut) {
         this.statut = statut;
+    }
+
+    @Override
+    public String toString() {
+        return "Paiement [idPaiement=" + idPaiement + ", idAbonnement=" + idAbonnement + ", dateEcheance=" + dateEcheance + ", datePaiement=" + datePaiement + ", typePaiement=" + typePaiement + ", statut=" + statut + "]";
     }
 }
