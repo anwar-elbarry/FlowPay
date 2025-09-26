8e7d003265a8a54bbeae8f0bff3baa8c611e6ac0
Anouar El barry <elbarryanwar37@gmail.com>
2025-09-26 16:07:11 +0100

    fit:complete principal functionalities

--- src/métier/PaiementService.java
+++ src/métier/PaiementService.java
@@ -1,9 +1,43 @@
 package métier;
 
+import JDBC.PaiementDAOJDBC;
+import entity.Paiement;
+
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.List;
+
 public class PaiementService {
-    public void enregistrer(){}
-    public void modifier(){}
-    public void supprimer(){}
-    public void detecterImpaye(){}
-    public void genererRapport(){}
+    private PaiementDAOJDBC paiementDAOJDBC;
+
+    public PaiementService(PaiementDAOJDBC paiementDAOJDBC) {
+        this.paiementDAOJDBC = paiementDAOJDBC;
+    }
+
+    public void enregistrer(Paiement p) {
+        this.paiementDAOJDBC.create(p);
+    }
+
+    public void modifier(Paiement p) throws SQLException {
+        try {
+            this.paiementDAOJDBC.modifier(p);
+        } catch (SQLException e) {
+            throw new SQLException(e.getMessage());
+        }
+    }
+
+    public void supprimer(String id) throws SQLException {
+        try {
+            this.paiementDAOJDBC.supprimer(id);
+        } catch (SQLException e) {
+            throw new SQLException(e.getMessage());
+        }
+    }
+
+    public List<Paiement> detecterImpaye() throws SQLException {
+        return this.paiementDAOJDBC.findUnpaid();
+    }
+
+    public void genererRapport() {
+    }
 }
