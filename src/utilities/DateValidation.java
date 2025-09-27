b56940072e7e923a45db6b126f23845efe94581b
Anouar El barry <elbarryanwar37@gmail.com>
2025-09-27 15:14:43 +0100

    fit:add compareDates function

--- src/utilities/DateValidation.java
+++ src/utilities/DateValidation.java
@@ -6,15 +6,20 @@ import java.util.Scanner;
 
 public class DateValidation {
     private static final Scanner sc = new Scanner(System.in);
-    public static LocalDate askDate(String message){
-        while (true){
+
+    public static LocalDate askDate(String message) {
+        while (true) {
             System.out.println(message);
-            String input  = sc.nextLine();
+            String input = sc.nextLine();
             try {
                 return LocalDate.parse(input);
-            }catch (DateTimeParseException e){
+            } catch (DateTimeParseException e) {
                 System.out.println("❌ Format invalide. Réessayez (ex: 2025-09-23).");
             }
         }
+    }
+
+    public static int compareDates(LocalDate d1, LocalDate d2) {
+        return d1.compareTo(d2);
     }
 }
