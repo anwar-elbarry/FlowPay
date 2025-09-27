1e0073f3f8e94499cd82bd6c35fbe5fb7e9b6967
Anouar El barry <elbarryanwar37@gmail.com>
2025-09-27 18:14:27 +0100

    fit:paiementAnalys

--- src/métier/PaiementService.java
+++ src/métier/PaiementService.java
@@ -2,10 +2,12 @@ package métier;
 
 import JDBC.PaiementDAOJDBC;
 import entity.Paiement;
+import utilities.PayStatut;
 
 import java.sql.SQLException;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class PaiementService {
     private PaiementDAOJDBC paiementDAOJDBC;
@@ -38,6 +40,16 @@ public class PaiementService {
         return this.paiementDAOJDBC.findUnpaid();
     }
 
-    public void genererRapport() {
+    public void paiementAnalys() throws SQLException {
+        try {
+            List<Paiement> unpaidPaiement = paiementDAOJDBC.findUnpaid();
+            List<Paiement> paidPaiement = paiementDAOJDBC.findAll().stream().filter(p -> p.getStatut().equals(PayStatut.PAYE)).collect(Collectors.toList());
+            List<Paiement> enRetardPaiement = paiementDAOJDBC.findAll().stream().filter(p -> p.getStatut().equals(PayStatut.EN_RETARD)).collect(Collectors.toList());
+            System.out.println("Nombre de paiements impayés : " + unpaidPaiement.size());
+            System.out.println("Nombre de paiements payés : " + paidPaiement.size());
+            System.out.println("Nombre de paiements en retard : " + enRetardPaiement.size());
+        } catch (SQLException e) {
+            System.out.println("Error while analyzing payments : " + e.getMessage());
+        }
     }
 }
